8bdeb43af9f9315d1280b24abf0f4fcabc183540
Wes Pickett <wes@krossover.com>
2015-02-12 22:16:43 -0500

    added market hours

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var express = require('express'),
 	fxAPI = require('./api.js')(),
 	tradingAlgorithm = require('./tradingAlgorithm.js')(fxAPI);
 
+//TODO: config variables
 var DEV=false;
 var WAIT_SECONDS = 60 * 1; //1 minute
 
@@ -26,7 +27,29 @@ server.on('connection', function(stream) {
 });
 
 var instruments = ['USD_CAD', 'EUR_USD'];
+var marketHours = {
+	'EUR_USD': {
+		//closed 4pm -> 1am
+		start: 6,
+		end: 21
+	},
+	'USD_CAD': {
+		//closed 7pm -> midnight
+		start: 5,
+		end: 24
+	}
+}
 var runCount = 0;
+
+function isMarketHours(instrument) {
+	var now = new Date();
+	
+	switch(instrument) {
+		case 'USD_CAD':
+		case 'EUR_USD':
+			return (now.getUTCHours() > marketHours[instrument].start && now.getUTCHours() < marketHours[instrument].end);
+	}
+}
 
 setInterval(function() {
 
@@ -40,8 +63,10 @@ setInterval(function() {
 			console.log('ERROR: fxApiRes data structure changed, or invalid response from the server');
 		} else {
 
-			//TODO: loop through multiple possible returned instruments
 			instruments.forEach(function(instrument) {
+
+				if (!isMarketHours(instrument)) return;
+
 				for (var i = 0; i < data.prices.length; i++) {
 					if (data.prices[i].instrument === instrument) {
 						var askPrice = parseFloat(data.prices[i].ask, 10);
